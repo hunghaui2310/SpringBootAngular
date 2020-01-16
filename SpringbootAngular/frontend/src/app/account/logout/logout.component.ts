937af9348115a4053648d98e7f8b1644d419cb4a
Hung <hunghaui2310@gmail.com>
2020-01-16 08:50:39 +0700

    commit

--- SpringbootAngular/frontend/src/app/account/logout/logout.component.ts
+++ SpringbootAngular/frontend/src/app/account/logout/logout.component.ts
@@ -18,7 +18,7 @@ export class LogoutComponent implements OnInit {
   }
   login() {
     this.authService.authenticate(this.user, (e) => {
-      this.router.navigateByUrl('/profile');
+      this.router.navigateByUrl('/home');
       console.log(e);
       let resp: any;
       resp = e.principal;
