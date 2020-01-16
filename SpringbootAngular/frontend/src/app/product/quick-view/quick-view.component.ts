785a5718d42df4d2286d663f60e7238ef2b140f6
Hung <hunghaui2310@gmail.com>
2020-01-16 13:47:39 +0700

    commit

--- SpringbootAngular/frontend/src/app/product/quick-view/quick-view.component.ts
+++ SpringbootAngular/frontend/src/app/product/quick-view/quick-view.component.ts
@@ -1,5 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {ProductService} from '../../service/product.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-quick-view',
@@ -9,16 +11,48 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 export class QuickViewComponent implements OnInit {
 
   constructor(private dialogRef: MatDialogRef<QuickViewComponent>,
-              @Inject(MAT_DIALOG_DATA) public data) {
+              @Inject(MAT_DIALOG_DATA) public data,
+              private productService: ProductService,
+              private route: ActivatedRoute) {
     dialogRef.disableClose = true;
+  }
+
+  productId;
+  dataProduct;
+  productName;
+  price;
+  numLike;
+  description;
+  cateName;
+  realPrice;
+  discount;
+  categoryId;
+
+  ngOnInit() {
+    console.log('view');
+    this.getProDetail();
   }
 
   closeForm() {
     this.dialogRef.close();
   }
 
-  ngOnInit() {
-    console.log('view');
+  getProDetail() {
+    this.productId = this.route.snapshot.params['id'];
+    console.log(this.productId);
+    this.productService.productDetailAPI(this.productId).subscribe(
+      data => {
+        console.log('data detail', data['data']);
+        this.dataProduct = data['data'];
+        this.productName = this.dataProduct['productName'];
+        this.price = this.dataProduct['price'];
+        this.numLike = this.dataProduct['numLike'];
+        this.description = this.dataProduct['description'];
+        this.cateName = this.dataProduct['categoryName'];
+        this.realPrice = this.dataProduct['realPrice'];
+        this.discount = this.dataProduct['discount'];
+        this.categoryId = this.dataProduct['categoryId'];
+        console.log('cateId', this.categoryId);
+      });
   }
-
 }
